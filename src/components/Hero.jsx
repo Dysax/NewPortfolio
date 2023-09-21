1b7b2c9efdb1f642637ac91c32413d3e02256663
Dysax <109558338+Dysax@users.noreply.github.com>
2023-09-21 17:13:10 -0500

    working on scroll icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,6 +31,10 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about"></a>
+      </div>
     </section>
   );
 };
